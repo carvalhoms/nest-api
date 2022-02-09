1041ce32b31ad3fcb39fdd10bc2a42c29bd4d085
Marcelo Carvalho <carvalhoms@icloud.com>
2022-02-08 23:14:35 -0300

    Criando rota post no controller users Rota exemplo create que cria um usuário

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,9 +1,14 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Post } from '@nestjs/common';
 
 @Controller('users')
 export class UsersController {
   @Get()
   getUsers() {
-    return 'Listando todos os usuários';
+    return 'Listando todos os usuários!!!';
+  }
+
+  @Post()
+  create() {
+    return 'Usuário criado com sucesso!!!';
   }
 }
