f4552d59ef43becc0fdc7ad5d4da787eedee7e69
Marcelo Carvalho <carvalhoms@icloud.com>
2022-02-09 23:03:17 -0300

    Criando o users.module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,11 @@
-import { UsersService } from './users/users.service';
+import { UsersModule } from './users/users.module';
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { UsersController } from './users/users.controller';
 
 @Module({
-  imports: [],
-  controllers: [AppController, UsersController],
-  providers: [UsersService, AppService],
+  imports: [UsersModule],
+  controllers: [AppController],
+  providers: [AppService],
 })
 export class AppModule {}
